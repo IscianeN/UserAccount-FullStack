476f7ed5cb6225d6c4ef12a70e274daa8be5f914
Isciane <91957387+IscianeN@users.noreply.github.com>
2022-01-26 16:52:17 +0100

    front end ended and start to mount a server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
 
+import React from "react"
 import './App.css';
 import Login from './components/Login';
 import Account from './components/Account';
+// import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-   <>{/* <Login></Login> */}<Account></Account></>
+   //<>{/* <Login></Login> */}<Account></Account></>
+   <div> 
+      {/* <Routes>
+        <Route exact path = "/" component = {Account} ></Route>
+      </Routes> */}
+      <BrowserRouter> 
+      <Account></Account>
+      <h1> Login </h1>
+      <Login></Login>
+      </BrowserRouter>
+   </div>
+   
   );
 }
 
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 export default class Login extends React.Component {
+
+    state = {
+        email : "",
+        password : ""
+    }
+
+    handleChange = (e) => {
+        const {name,value} = e.target
+        this.setState({[name] : value})
+    }
+
+
+    handleSubmit = (e) => {
+       
+    }
     render() {
         return (
             <>
             <div>
-                <form>
-                    <input type = "email" name = "email" placeholder = "email..." required></input>
-                    <input type ="password" name = "password" placeholder = "password..." required></input>
+                <form onSubmit={this.handleSubmit}>
+                    <input type = "email" name = "email" placeholder = "email..." required onChange={this.handleChange}></input>
+                    <input type ="password" name = "password" placeholder = "password..." required onChange={this.handleChange}></input>
+                    <button onSubmit={this.handleSubmit}>Login </button>
                 </form>
             </div>
             </>
